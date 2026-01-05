746a480fdfdc36f7138ee01d9235693550698137
ssalomonsus-oss <ssalomonsus@gmail.com>
2026-01-05 12:52:04 +0100

    Create SettingsFragment.kt

---
+++ app/src/main/java/com/ncorti/kotlin/template/app/SettingsFragment.kt
@@ -0,0 +1,33 @@
+package com.ncorti.kotlin.template.app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val switchDark = view.findViewById<SwitchCompat>(R.id.switch_dark)
+        // initialize switch with current mode
+        switchDark.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        switchDark.setOnCheckedChangeListener { _, isChecked ->
+            val mode = if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            AppCompatDelegate.setDefaultNightMode(mode)
+            activity?.recreate()
+        }
+    }
+}
